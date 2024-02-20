9f4278d7d891a1994028b317bfe5d87b8db6930d
mobilepig <mobilepig@naver.com>
2024-02-20 17:37:04 +0900

    2024/02/20 / leaderboard first commit / sheep changsuk

--- server.js
+++ server.js
@@ -46,4 +46,69 @@ app.post("/postest", (req, res) => {
   res.json(req.body);
 });
 
+app.get("/data", async (req, res, next) => {
+  let client;
+  try {
+    client = await pool.connect();
+
+    const likeQuery = `
+      SELECT post_id, user_id, title, like_count, desti_name, revisit_count, area, travel_type, desti_type, thumbnail_url
+      FROM posts
+      ORDER BY like_count DESC
+      LIMIT 10;
+    `;
+
+    const revisitQuery = `
+      SELECT post_id, user_id, title, like_count, desti_name, revisit_count, area, travel_type, desti_type, thumbnail_url
+      FROM posts
+      ORDER BY revisit_count DESC
+      LIMIT 10;
+    `;
+
+    const elderQuery = `
+      SELECT post_id, user_id, title, like_count, desti_name, revisit_count, area, travel_type, desti_type, thumbnail_url
+      FROM posts
+      WHERE travel_type = 'elder'
+      ORDER BY like_count DESC
+      LIMIT 10;
+    `;
+
+    const kidQuery = `
+    SELECT post_id, user_id, title, like_count, desti_name, revisit_count, area, travel_type, desti_type, thumbnail_url
+    FROM posts
+    WHERE travel_type = 'kid'
+    ORDER BY like_count DESC
+    LIMIT 10;
+  `;
+
+    const likeResult = await client.query(likeQuery);
+    const revisitResult = await client.query(revisitQuery);
+    const elderResult = await client.query(elderQuery);
+    const kidResult = await client.query(kidQuery);
+    console.log(likeResult.rows);
+    // 결과를 JSON 형태로 클라이언트에 응답
+    res.json({
+      like: likeResult.rows,
+      revisit: revisitResult.rows,
+      elder: elderResult.rows,
+      kid: kidResult.rows,
+    });
+  } catch (error) {
+    console.error("Error executing query:", error.message);
+    res.status(500).json({ error: "db error" });
+  } finally {
+    if (client) {
+      client.release(); // 클라이언트 반환
+    }
+  }
+});
+
+// 애플리케이션이 종료될 때 풀을 명시적으로 종료
+process.on("SIGINT", () => {
+  pool.end().then(() => {
+    console.log("Pool has ended");
+    process.exit(0);
+  });
+});
+
 app.listen(port, () => console.log("Server is running on : " + port));
